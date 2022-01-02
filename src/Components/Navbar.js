c0bf16f0c553e495d9591a0bfaf32f0e86476402
Saige Cross <saigecrossdev@gmail.com>
2022-01-01 19:01:36 -0600

    testing netlify deploy

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
     return (
         <div>
             <nav className="navbar">
-                <Link className="navLink" to="/">Home</Link>
+                <Link className="navLink" to="/">Home (test)</Link>
                 <Link className="navLink" activeStyle={{ color: "rgb(38, 148, 171)" }} activeClassName="active" to="/shop">Shop</Link>
                 <Link className="navLink" activeStyle={{ color: "rgb(38, 148, 171)" }} activeClassName="active" to="/contact">Contact</Link>
                 <Link className="navLink" activeStyle={{ color: "rgb(38, 148, 171)" }} activeClassName="active" to="https://www.instagram.com/maisiepaigecross/?hl=en">Social</Link>
